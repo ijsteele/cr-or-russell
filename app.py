658011a0073719f66cbc69adfd7a16f5d3c133e7
ian <94004131+ijsteele@users.noreply.github.com>
2021-11-17 14:51:44 -0500

    Add files via upload
    Renamed 'template.html' to 'homepage.html'
    Centered the h1

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import render_template
 app = Flask('cr_russell')
 
 @app.route('/')
-def hello():
-    return render_template("template.html")
+def home():
+    return render_template("homepage.html")
 
 if __name__ == "__main__":
     app.run()
