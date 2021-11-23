76f4fc956f85b5b3a9cbd61d6fab76c5755d116e
StandardGage <gagedolph@yahoo.com>
2021-11-23 14:15:15 -0500

    Added Google Login Functionality

--- app.py
+++ app.py
@@ -1,11 +1,121 @@
-from flask import Flask
-from flask import render_template
+import json
+import os
+import sqlite3
+
+from flask import Flask, config, redirect, request, url_for, render_template
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_manager,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+from werkzeug import exceptions
+
+from db import init_db_command
+import db
+from user import User
+
+GOOGLE_CLIENT_ID = "577886556164-i4da8abclqef1igo5icgu0inng0p3227.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
 
 app = Flask('cr_russell')
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@app.before_first_request
+def create():
+    try:
+        db.init_db()
+    except sqlite3.OperationalError:
+        pass
+
+
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
 
 @app.route('/')
 def home():
-    return render_template("homepage.html")
+    if current_user.is_authenticated:
+        return render_template("homepage.html")
+    else:
+        return '<a class="button" href="/login">Google Login</a>'
 
-if __name__ == "__main__":
-    app.run()
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+
+@app.route('/login')
+def login():
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg['authorization_endpoint']
+
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+
+@app.route('/login/callback')
+def callback():
+    code = request.args.get('code')
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    user = User(
+        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+    )
+
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+
+    login_user(user)
+
+    return redirect(url_for("home"))
+
+
+# if __name__ == "__main__":
+    # app.run(ssl_context='adhoc')
